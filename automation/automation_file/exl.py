c41dcea1081014004716b3397298d85a3c26de59
sceps <bbaidoo72@gmail.com>
2021-07-08 17:28:44 -0400

    "connectionfile"

--- automation/automation_file/exl.py
+++ automation/automation_file/exl.py
@@ -29,4 +29,26 @@ cisco_xr ={'device_type': 'cisco_xr_ssh', 'ip': '10.10.10.27', 'username': 'admi
 arista_veos = {'device_type':'arista_eos_ssh', 'username': 'admin', 'password':'passwd', 'secret': '', 'port': 8622,}
 
 
+# network code
+
+from datetime import datetime
+from netmiko import ConnectHandler
+from my_devices import cisco_ios, cisco_xr, arista_veos
+
+
+def main():
+    device_list = [cisco_ios, cisco_xr, arista_veos]
+    start_time = datetime.now()
+    for a_device in device_list:
+        net_connect = ConnectHandler(**a_device)
+        print("{}: {}".format(net_connect.device_type, net_connect.find_prompt()))
+    print("Time elapsed:  {}\n".format(datetime.now()))
+    if check_bgp(net_connect):
+        print("BGP currently configured")
+    else:
+        print("No BGP")
+        print("Time elapse: {}\n".format(datetime.now() - start_time))
+
+if __name__ == "__main__":
+    main()
 
